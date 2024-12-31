aa76504b55c8ac7088554309068ec02d611e9205
escottalexander@gmail.com <escottalexander@gmail.com>
2024-12-31 11:27:19 -0500

    add plausible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { Copy } from './components/copy'
 import { Leaderboard } from './components/leaderboard'
+import PlausibleProvider from 'next-plausible'
 
 function GitIcon() {
   return (
@@ -48,48 +49,50 @@ function TrophyIcon() {
 
 export default function Home() {
   return (
-    <div className="font-sourceCodePro">
-      <div className="bg-screen min-h-screen px-6">
-        <div className="relative mx-auto max-w-2xl py-32 sm:py-36">
-          <div className="text-center">
-            <div className="flex justify-center items-center relative font-sourceCodePro text-balance text-5xl font-bold tracking-tight sm:text-7xl">
-              <div aria-hidden="true" className="glitch-before">
-                ETH TECH TREE
+    <PlausibleProvider domain="ethtechtree.com">
+      <div className="font-sourceCodePro">
+        <div className="bg-screen min-h-screen px-6">
+          <div className="relative mx-auto max-w-2xl py-32 sm:py-36">
+            <div className="text-center">
+              <div className="flex justify-center items-center relative font-sourceCodePro text-balance text-5xl font-bold tracking-tight sm:text-7xl">
+                <div aria-hidden="true" className="glitch-before">
+                  ETH TECH TREE
+                </div>
+                <h1 className="glitch-text">ETH TECH TREE</h1>
+                <div aria-hidden="true" className="glitch-after">
+                  ETH TECH TREE
+                </div>
               </div>
-              <h1 className="glitch-text">ETH TECH TREE</h1>
-              <div aria-hidden="true" className="glitch-after">
-                ETH TECH TREE
-              </div>
+              <p className="mt-4 text-pretty text-lg font-medium sm:text-xl/8">
+                Advanced Solidity coding challenges to test your Ethereum
+                development skills.
+              </p>
             </div>
-            <p className="mt-4 text-pretty text-lg font-medium sm:text-xl/8">
-              Advanced Solidity coding challenges to test your Ethereum
-              development skills.
-            </p>
-          </div>
-          <div className="mt-12 border border-gray-700 rounded-md">
-            <div className="px-4 py-3 flex items-center justify-between text-white">
-              <div className="font-semibold">&gt;_ Terminal</div>
-              <div className="flex gap-4">
-                <Link href="#leaderboard">
-                  <TrophyIcon />
-                </Link>
-                <Link href="https://github.com/BuidlGuidl/eth-tech-tree">
-                  <GitIcon />
-                </Link>
-                <Copy />
+            <div className="mt-12 border border-gray-700 rounded-md">
+              <div className="px-4 py-3 flex items-center justify-between text-white">
+                <div className="font-semibold">&gt;_ Terminal</div>
+                <div className="flex gap-4">
+                  <Link href="#leaderboard">
+                    <TrophyIcon />
+                  </Link>
+                  <Link href="https://github.com/BuidlGuidl/eth-tech-tree">
+                    <GitIcon />
+                  </Link>
+                  <Copy />
+                </div>
               </div>
-            </div>
-            <div className="p-4 bg-slate-800 border-t border-gray-700 rounded-b-md">
-              <pre className="text-lg">
-                <code>
-                  npx <span className="text-green-400">eth-tech-tree</span>
-                </code>
-              </pre>
+              <div className="p-4 bg-slate-800 border-t border-gray-700 rounded-b-md">
+                <pre className="text-lg">
+                  <code>
+                    npx <span className="text-green-400">eth-tech-tree</span>
+                  </code>
+                </pre>
+              </div>
             </div>
           </div>
         </div>
+        <Leaderboard />
       </div>
-      <Leaderboard />
-    </div>
+    </PlausibleProvider>
   )
 }
